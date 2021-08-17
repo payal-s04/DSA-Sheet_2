5b5cf5168c2f35d9d4f65807a0d28074ca65e4b6
payal-s04 <87601475+payal-s04@users.noreply.github.com>
2021-08-17 18:22:45 +0530

    Update K-th_digit_from_end_of_a_number.cpp

--- Maths/K-th_digit_from_end_of_a_number.cpp
+++ Maths/K-th_digit_from_end_of_a_number.cpp
@@ -1,1 +1,39 @@
+/*
+Problem 1 :
+You are given t number of testcases. Each testcase contains a number n and k.
+Write a program to find kth digit from the end of a given number n.
 
+Input :
+ n k
+Output :
+ -1        (if n <= 0)
+ kth digit (otherwise)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        long long int n, k;
+        cin >> n >> k;
+        
+        long long int d = pow(10, k - 1);
+        
+        if((n <= 0) || (n < d))
+        {
+            cout << "-1" << endl;
+        }
+        else
+        {
+            cout << (n / d) % 10 << endl;
+        }
+    }
+    
+    return 0;
+}
