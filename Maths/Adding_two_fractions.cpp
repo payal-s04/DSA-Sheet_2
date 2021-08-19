d07c700d4632247861a1fe36adaa6494b441eaf4
payal-s04 <87601475+payal-s04@users.noreply.github.com>
2021-08-19 12:40:20 +0530

    Create Adding_two_fractions.cpp

---
+++ Maths/Adding_two_fractions.cpp
@@ -0,0 +1,12 @@
+/*
+Problem 14:
+
+You are given t number of testcases. Each testcase contains two fractions a and b.
+Write a program to add given two fractions.
+
+E.g.
+ Input:
+ 1/3 3/9
+ Output:
+ 2/3 (take numerators and denominators separately as input and give the simplified fraction of the sum as the output)
+*/
