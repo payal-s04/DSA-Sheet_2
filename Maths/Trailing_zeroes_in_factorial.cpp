8e134524112da0062943ca187de14eeec59f0b21
payal-s04 <87601475+payal-s04@users.noreply.github.com>
2021-08-21 08:51:22 +0530

    Create Trailing_zeroes_in_factorial.cpp

---
+++ Maths/Trailing_zeroes_in_factorial.cpp
@@ -0,0 +1,60 @@
+/*
+Problem 18:
+
+You are given t number of testcases. Each testcase contains a number n.
+For an integer n find the number of trailing zeroes in n!.
+
+E.g.
+ Input:
+ 5
+ Outuput:
+ 1 (5! = 120)
+*/
+
+#include<iostream>
+using namespace std;
+
+int trailingZeroes(int n)
+{
+    long long int fact = 1;
+    
+    for(int i = 2; i <= n; i++)
+    {
+        fact *= i;
+    }
+    
+    int count = 0;
+    
+    while(fact != 0)
+    {
+        if(fact % 10 != 0)
+        {
+            break;
+        }
+
+        if(fact % 10 == 0)
+        {
+            count++;
+        }
+        
+        fact /= 10;
+    }
+    
+    return count;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        cout << trailingZeroes(n) << endl;
+    }
+    
+    return 0;
+}
