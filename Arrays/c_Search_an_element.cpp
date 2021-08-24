471f2504258de966b2a8760e17411cb18ce364f1
Payal Sharma <87601475+payal-s04@users.noreply.github.com>
2021-08-24 13:42:20 +0530

    Create c_Search_an_element.cpp

---
+++ Arrays/c_Search_an_element.cpp
@@ -0,0 +1,42 @@
+/*
+Problem 13:
+
+Given a matrix mat[][] of size N x M, where every row and column is sorted in increasing order, and a number X is given.
+The task is to find whether element X is present in the matrix or not.
+
+E.g.
+ Input:
+ 3 3
+ 3 30 38 
+ 44 52 54 
+ 57 60 69
+ 62
+ Output:
+ 0
+ 
+ Input:
+ 1 6
+ 18 21 27 38 55 67
+ 55
+ Output:
+ 1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n >> m;
+        cin >> n >> m;
+        
+        
+    }
+    
+    return 0;
+}
