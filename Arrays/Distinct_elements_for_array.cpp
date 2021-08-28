b4aed978a906b8ee28f22381ada8a60d3a895081
Payal Sharma <87601475+payal-s04@users.noreply.github.com>
2021-08-28 21:40:13 +0530

    Update Distinct_elements_for_array.cpp

--- Arrays/Distinct_elements_for_array.cpp
+++ Arrays/Distinct_elements_for_array.cpp
@@ -11,3 +11,57 @@ E.g.
  1 3 4 8
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> requiredDigits(vector<int> nums)
+{
+    vector<int> m;
+    for (auto i : nums)
+    {
+        while (i != 0)
+        {
+            int n = i % 10;
+            vector<int>::iterator it = find(m.begin(), m.end(), n);
+            if (it == m.end())
+            {
+                m.push_back(n);
+            }
+            i /= 10;
+        }
+    }
+
+    sort(m.begin(), m.end());
+
+    return m;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v;
+        for(int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            v.push_back(a);
+        }
+
+        vector<int> vec = requiredDigits(v);
+
+        for(auto i : vec)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
